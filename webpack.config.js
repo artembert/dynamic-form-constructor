0f5e718262ad1014f493ead83e50f7abc5149c83
Artem Beresnev <artembert@gmail.com>
2022-10-13 19:48:46 +0300

    Run dev server

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+// @ts-check
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const DotenvWebpackPlugin = require("dotenv-webpack");
+
+const isProd = process.env.NODE_ENV === "production";
+
+/**
+ @type {import('webpack').Configuration} Configuration
+ */
+module.exports = {
+  mode: isProd ? "production" : "development",
+  devtool: isProd ? undefined : "inline-source-map",
+  devServer: {
+    port: 1234,
+    historyApiFallback: true,
+  },
+  entry: path.resolve(__dirname, "./src/index.tsx"),
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js",
+    publicPath: "/",
+    clean: true,
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: [
+          "babel-loader",
+          {
+            loader: "ts-loader",
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+    }),
+    new MiniCssExtractPlugin(),
+    new DotenvWebpackPlugin(),
+  ],
+};
