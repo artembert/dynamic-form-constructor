5f9d93577a0ddff1cf6a58d020c7147628912d78
Artem Beresnev <artembert@gmail.com>
2022-10-16 12:35:42 +0300

    Add typed hooks useAppSelector & useAppDispatch

--- src/containers/output-container/index.tsx
+++ src/containers/output-container/index.tsx
@@ -1,12 +1,11 @@
-import { useDispatch, useSelector } from "react-redux";
 import { StrictMode, useCallback } from "react";
 import { Output } from "../../components/output";
 import { changeFormField } from "../../store/features/form";
-import { RootState } from "../../store";
+import { useAppDispatch, useAppSelector } from "../../hooks";
 
 export const OutputContainer = () => {
-  const dispatch = useDispatch();
-  const config = useSelector((state: RootState) => state.form.fields);
+  const dispatch = useAppDispatch();
+  const config = useAppSelector((state) => state.form.fields);
   const fieldChangeHandler = useCallback(
     (name, value) => dispatch(changeFormField({ name, value })),
     [dispatch, changeFormField]
