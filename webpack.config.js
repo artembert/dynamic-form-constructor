b65b219964a0cff4e8238b5325e3cd1e24d20ac2
Artem Beresnev <artembert@gmail.com>
2022-10-18 20:26:58 +0300

    Resolve resources from page root

--- webpack.config.js
+++ webpack.config.js
@@ -47,6 +47,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "public/index.html",
+      baseUrl: isProd ? "./" : "/",
     }),
     new MiniCssExtractPlugin(),
     new DotenvWebpackPlugin(),
